c7857dc5649290fa5a7348c2b0a784fa4eb91ec0
Seolhee Kim <seolheekim808@gmail.com>
2019-02-13 14:00:18 -0800

    installed lodash and refactoring action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../api/jsonPlaceholder';
 
 // Action Creator is a function that creates or
 // returns a plain JS object to create 'actions'
 
 //Action Creator
+export const fetchPostsAndUsers= () => async dispatch => {
+  // await key is going to essentially make sure that it waits for the API request to be
+  // completed before it moves on and does anything else.
+  await dispatch(fetchPosts())
+}; // end of fetchPostsAndUsers()
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts')
 
@@ -21,7 +28,6 @@ export const fetchUser = id => async dispatch => {
     payload: response.data
   })
 }; // end of fetchUser()
-
 
 // *** this is before refactoring the code ***
 //  export const fetchPosts = () => {
